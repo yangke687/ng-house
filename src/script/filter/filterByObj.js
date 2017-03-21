1310441f6cd322fb6a2fae8809e96696e64f9de6
Yang Ke <saint_yk@163.com>
2017-03-21 11:44:33 +0800

    remove debug info

--- src/script/filter/filterByObj.js
+++ src/script/filter/filterByObj.js
@@ -5,8 +5,6 @@ angular.module('myApp')
 			angular.forEach(list, function(item) {
 				var isEqual = true;
 				for (var e in filterObj) {
-					console.log(item);
-					console.log('e', e, 'filter:', filterObj[e], 'item', item[e]);
 					if (filterObj[e] !== item[e]) {
 						isEqual = false;
 					}
