581b05fc719559c1db4a851e0912350b4e7ee363
Yang Ke <saint_yk@163.com>
2017-03-24 11:23:53 +0800

    house page,binding history go back event

--- src/script/directive/houseInfo.js
+++ src/script/directive/houseInfo.js
@@ -4,5 +4,10 @@ angular.module('myApp')
 			restrict: 'A',
 			replace: true,
 			templateUrl: 'view/template/houseInfo.html',
+			link: function(scope) {
+				scope.back = function() {
+					window.history.go(-1);
+				}
+			}
 		};
 	}]);
