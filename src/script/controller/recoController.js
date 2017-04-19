bb56da3a7e15d0632c0425cdbf5c577fb7c3fd84
YangKe <saint_yk@163.com>
2017-04-19 11:24:49 +0800

    house recommendation list fetch data

--- src/script/controller/recoController.js
+++ src/script/controller/recoController.js
@@ -1,9 +1,9 @@
 angular.module('myApp')
 	.controller('recoController',['$http','$scope',function($http,$scope){
 		$scope.list = [];
-		$http.get('/data/houseList.json')
+		$http.get($scope.backendUrlBase+'/houseList.do?rows=15&page=1')
 			.then(function(res) {
-				$scope.list = res.data;
+				$scope.list = res.data.list;
 			})
 			.then(function(err) {
 				// handle err
